9b7121ec566a35b1578b1dee2da10e1eda91a3f4
pitillus <67270750+pitillus@users.noreply.github.com>
2020-06-23 09:29:07 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+  return "Bienvenido 2!!!"
+  
+@app.route('/como estas')
+def hola():
+  return 'Estoy bien, gracias'
+  
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=8080)
